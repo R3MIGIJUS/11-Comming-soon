ed89fdf1e9f5c3f188dd80636d6b5aa225a1943d
R3MIGIJUS <remigijus.builys@gmail.com>
2022-05-12 14:32:00 +0300

    Render Socials

---
+++ js/data/socialsData.js
@@ -0,0 +1,44 @@
+const socialsData = [
+    {
+        href: '#',
+        icon: 'facebook',
+       
+    },
+    {
+        href: '#',
+        icon: 'google-plus',
+        
+    },
+    {
+        href: '#',
+        icon: 'twitter',
+        
+    },
+    {
+        href: '#',
+        icon: 'dribbble',
+        
+    },
+    {
+        href: '#',
+        icon: 'tumblr',
+        
+    },
+    {
+        href: '#',
+        icon: 'linkedin',
+       
+    },
+    {
+        href: '#',
+        icon: 'instagram',
+       
+    },
+    {
+        href: '#',
+        icon: 'rss',
+       
+    }
+];
+
+export { socialsData }
