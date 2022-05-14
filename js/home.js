e856e2fc824e264d4bbb48c7f59f938b91a004f4
R3MIGIJUS <remigijus.builys@gmail.com>
2022-05-14 23:59:14 +0300

    Clock as class

--- js/home.js
+++ js/home.js
@@ -4,9 +4,6 @@ import { Clock } from "./components/clock/Clock.js";
 import { socials } from "./components/socials/socials.js";
 import { socialsData } from "./data/socialsData.js";
 
-
-
-
 //EXECUTION
-Clock ('#clock_1'); 
-socials ('footer .socials',socialsData);
+new Clock("#clock_1"); // Norint paleisti objektinį javaScript turinį rašome :"new" prieš objektą.
+socials("footer .socials", socialsData);
